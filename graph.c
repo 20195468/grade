9a3da57e80c46bb1bfa11f57f2c7a6dc0d220847
lixingze <1873798269@qq.com>
2020-12-08 11:05:02 +0800

    错误更改

--- graph.c
+++ graph.c
@@ -38,7 +38,6 @@ void find() {
             if (is[i] == 0 && distance[i] <= max) {
                 u = i;
                 max = distance[i];
-                printf("no change")
             }
         }
         is[u] = 1;
